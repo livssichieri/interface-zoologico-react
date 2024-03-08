81b14ca74ad62a29f0f4f3545253fa0f40fb71c5
Lívia Carolina <liviacarolinasouzasicchieri@gmail.com>
2024-03-08 11:19:36 -0300

    Challenge 08/03/2024

--- src/components/Atracao/Atracao.jsx
+++ src/components/Atracao/Atracao.jsx
@@ -1,13 +1,13 @@
-import imgNotFound from '../../assets/img-not-found.png'
+import imgNotFound from '../../assets/img-not-found.png'; // Importando recurso para o componente
 import './Atracao.css'
 
-function Atracao(){
-    return(
+function Atracao() {
+    return (
         <div className="ctn-atracao">
-            <img src={imgNotFound}/>
+            <img src={imgNotFound} />
             <p>Lorem Ipsum</p>
         </div>
-    );    
+    );
 }
-    
+
 export default Atracao;
--- src/components/Navegacao/Navegacao.jsx
+++ src/components/Navegacao/Navegacao.jsx
@@ -1,23 +1,36 @@
-import './Navegacao.css'
+import './Navegacao.css';
+import { Link } from 'react-router-dom';
 
-function Navegacao(){
+function Navegacao() {
+
     return(
-        <div className="barra-navegacao"> 
+        <div className="barra-navegacao">
             <div className="logo-zoo">
                 <h1>ZOOLÓGICO</h1>
             </div>
 
             <div className="navbar-opcoes">
                 <ul>
-                    <li>Início</li>
-                    <li>Os animais</li>
-                    <li>Fotos</li>
-                    <li>Depoimentos</li>
-                    <li>Login</li>
+                    
+                    <li>
+                        <Link style= {{color: "#A7A000"}} to= "/">Início</Link>
+                    </li>
+                    <li>
+                        <Link style= {{color: "#A7A000"}} to= "/animais">Os Animais</Link>
+                    </li>
+                    <li>
+                        <Link style= {{color: "#A7A000"}} to= "/fotos">Fotos</Link>
+                    </li>
+                    <li>
+                        <Link style= {{color: "#A7A000"}} to= "/depoimentos">Depoimentos</Link>
+                    </li>
+                    <li>
+                        <Link style= {{color: "#A7A000"}} to= "/login">Login</Link>
+                    </li>
                 </ul>
             </div>
         </div>
-    )
+    );
 }
- 
+
 export default Navegacao;
